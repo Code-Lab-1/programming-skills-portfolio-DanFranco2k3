e9477799ae49884820b618e023784d4251e351d0
Drexxty <70067132+Drexxty@users.noreply.github.com>
2022-10-10 17:36:00 +0400

    hi

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise 1.py
@@ -0,0 +1,7 @@
+print("Exerise 1 - Print Strings")
+print()
+print("""Twinkle, twinkle, little star,\t\n How I wonder what you are!"
+\t Up above the world so high, 
+\t Like a diamond in the sky. 
+Twinkle, twinkle, little star,
+\r How I wonder what you are""")
