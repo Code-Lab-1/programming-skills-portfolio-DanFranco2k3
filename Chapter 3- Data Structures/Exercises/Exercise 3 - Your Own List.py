18ec6ae949eb25b09366a9468d7bb0cd3bc2d8de
Drexxty <70067132+Drexxty@users.noreply.github.com>
2022-10-28 02:55:14 +0400

    Stages and Orders

---
+++ Chapter 3- Data Structures/Exercises/Exercise 3 - Your Own List.py
@@ -0,0 +1,9 @@
+car_brand_list = ['Koenigsegg','Lamborghini','Ferrari','Rolls Royce','Bentley','Chrysler']
+
+print("I would like to own a " + car_brand_list[1] +".")
+print()
+print("I would like to ride a " + car_brand_list[3] + ".")
+print()
+print("I would like to sell a " + car_brand_list[4] + ".")
+print()
+print("I would like to smash a " + car_brand_list [0] + ".")
---
+++ Chapter 3- Data Structures/Exercises/Exercise 4 - Guest List.py
@@ -0,0 +1,9 @@
+guests = ["Dan","Keil","Andrei","Adolfo","Franco"]
+
+print(guests[0] + ", please come to dinner.")
+print()
+print(guests[1] + ", please come to dinner.")
+print()
+print(guests[2] + ", please come to dinner.")
+print()
+print(guests[3] + ", please come to dinner.")
